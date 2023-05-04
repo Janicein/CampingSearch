3803ddd5dd58fe2cedc8278f4df5326fe8820a1f
Jiaming Zhou <zhou.jiam@northeastern.edu>
2023-05-04 14:24:17 -0700

    Set up logout

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,24 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User= require('../models/user');
+const {storeReturnTo} = require('../middleware');
 
 router.get('/register',(req,res)=>{
     res.render('users/register');
 })
 
 
-router.post('/register',catchAsync(async(req,res)=>{
+router.post('/register',catchAsync(async(req,res,next)=>{
     try{
     const{email,username,password} = req.body;
     const user = new User({email,username});
     const registeredUser = await User.register(user,password);
-    req.flash('success','Welcome');
-    res.redirect('/campgrounds');
+    req.login(registeredUser,err=>{
+        if(err) return next(err);
+        req.flash('success','Welcome');
+        res.redirect('/campgrounds');
+
+    })
     }catch(e){
         req.flash('error',e.message);
         res.redirect('/register');
@@ -28,9 +33,19 @@ router.get('/login',(req,res)=>{
 
 })
 
-router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{
+router.post('/login',storeReturnTo,passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{
     req.flash('success','Welcome Back!');
-    res.redirect('/campgrounds')
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout',(req,res)=>{
+    req.logout(req.user, err => {
+        if(err) return next(err);
+        req.flash('success','Goodbye!');
+        res.redirect("/campgrounds");
+      });
+
 })
 
 
